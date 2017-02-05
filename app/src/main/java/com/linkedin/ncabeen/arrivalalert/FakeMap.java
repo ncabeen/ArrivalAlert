16e75bfb165b8ee6ae844003b860ee502398e302
ncabeen <ncabeen@gmail.com>
2017-02-04 21:47:23 -0800

    Added second activity - really this is just a test commit.

---
+++ app/src/main/java/com/linkedin/ncabeen/arrivalalert/FakeMap.java
@@ -0,0 +1,22 @@
+package com.linkedin.ncabeen.arrivalalert;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class FakeMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_map);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/com/linkedin/ncabeen/arrivalalert/MainActivity.java
+++ app/src/main/java/com/linkedin/ncabeen/arrivalalert/MainActivity.java
@@ -1,6 +1,7 @@
 package com.linkedin.ncabeen.arrivalalert;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -37,8 +38,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, FakeMap.class);
+                startActivity(intent);
+
             }
         });
 
