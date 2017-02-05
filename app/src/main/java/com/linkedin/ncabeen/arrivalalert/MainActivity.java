183ef85e856e4b32a560b4fb12d21b5d26802740
ncabeen <ncabeen@gmail.com>
2017-02-04 22:04:29 -0800

    Added basic map activity and updated license

--- app/src/main/java/com/linkedin/ncabeen/arrivalalert/MainActivity.java
+++ app/src/main/java/com/linkedin/ncabeen/arrivalalert/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 //        .setAction("Action", null).show();
-                Intent intent = new Intent(MainActivity.this, FakeMap.class);
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
 
             }
